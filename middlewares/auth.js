fb797589d55211a00d21817615fdce0524ec82ce
paolojoaquinp <paolojoaquinpintoperez@gmail.com>
2023-05-02 22:57:10 -0400

    changes test

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,7 +4,7 @@ export default {
         if(!req.headers.token){
             return res.status(404).send({
                 message: 'No token'
-            });
+            }); 
         }
         const response = await tokenService.decode(req.headers.token);
         if(response.rol == 'Administrador' || response.rol == 'Vendedor' || response.rol == 'Almacenero'){
