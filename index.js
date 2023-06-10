773b6d8b6cb3c6d4f6af42722d1d4eff53b23a7f
paolojoaquinp <paolojoaquinpintoperez@gmail.com>
2023-06-10 15:38:15 -0400

    changes url

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ import router from './routes';
 //Conexion a la base de datos MongoDB
 mongoose.Promise=global.Promise;
 //const dbUrl = 'mongodb://localhost:27017/dbsistema';
-const dbUrl = 'mongodb+srv://paolojoa:5PR3ynWQPHhvQJNM@paolop.hg1l7.mongodb.net/dbsistema?retryWrites=true&w=majority';
+//const dbUrl = 'mongodb+srv://paolojoa:5PR3ynWQPHhvQJNM@paolop.hg1l7.mongodb.net/dbsistema?retryWrites=true&w=majority';
+const dbUrl = 'mongodb+srv://paolojoa:5PR3ynWQPHhvQJNM@cluster0.juudsic.mongodb.net/?retryWrites=true&w=majority';
 mongoose.connect(dbUrl, {useCreateIndex:true, useNewUrlParser: true})
 .then(mongoose => console.log('Conectado a la BD en el puerto 27017'))
 .catch(err => console.log(err));
